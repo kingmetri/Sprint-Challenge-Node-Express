93af32f4be5e96c11036d0ebb296e1c10857e1ca
kingmetri <demetribethel@gmail.com>
2019-01-01 22:52:07 -0500

    get requests done.

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const dbProjects = require('./data/helpers/projectModel');
 const PORT = process.env.PORT || 5000;
 
 server.get('/api/actions', (req,res) => {
-      dbActions.get()
+      dbActions.get(id)
                .then(action => {
                    res.json(action);
                })
@@ -17,6 +17,25 @@ server.get('/api/actions', (req,res) => {
                    res.json(500).json({errorMessage: "Unable to get actions this time"});
                })
 });
+
+server.get('/api/actions/:id', (req,res) => {
+    const { id } = req.params;
+    dbActions.get(id)
+             .then(action => {
+                action ? res.json(action) : res.status(404).json({Message: "Required action with this ID not found"});
+                console.log(action);
+             })
+             .catch(err => {
+                 res.json(500).json({errorMessage: "Unable to get actions this time"});
+             });
+});
+
+
+
+
+
+
+
 
 server.listen(PORT, () => {
     console.log(`App is up and running at the PORT ${PORT}.`);
